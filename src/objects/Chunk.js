89c1703019b10f6d9fd59307e69e34eaa27b8350
Steven Estes <Tapestes@gmail.com>
2018-12-11 12:32:24 -0500

    Update Chunk.js
    Prep for line selection from chunk

--- src/objects/Chunk.js
+++ src/objects/Chunk.js
@@ -1,10 +1,11 @@
 ﻿class Chunk {
-	constructor(chnkNm, addTime, stckHght, rhrsls, prbbltyOfRcll, clr) {
+	constructor(chnkNm, addTime, stckHght, rhrsls, prbbltyOfRcll, lineNo) {
 		this.chunkName = chnkNm;
 		this.addedAt = addTime;
 		this.stackDepthAtPush = stckHght;
 		this.rehearsals = rhrsls;
 		this.probabilityOfRecall = prbbltyOfRcll;
 		this.color = clr;
+        this.lineNumber = lineNo;
 	}
 }
