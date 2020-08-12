4de8569db568d37a1c47da3dc8b7123e81953ea4
Steven Estes <Tapestes@gmail.com>
2020-08-12 14:32:07 -0400

    Update SettingsManager.js
    Added settings for line numbers

--- src/managers/SettingsManager.js
+++ src/managers/SettingsManager.js
@@ -5,6 +5,7 @@ class SettingsManager{
     constructor() {
         if (config.get('darkMode') == undefined) config.set('darkMode', false);
         if (config.get('sidebarWidth') == undefined) config.set('sidebarWidth', 190);
+        if (config.get('lineNumbers') == undefined) config.set('lineNumbers', false);
     }
     
     setSetting(key, value){
