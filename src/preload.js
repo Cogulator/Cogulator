e0c9f242f14af9a3919c31c7bc1ae1250d94d02a
Steven Estes <Tapestes@gmail.com>
2022-07-11 15:15:29 -0400

    Put back draggable area for titlebar in macos

--- src/preload.js
+++ src/preload.js
@@ -2,6 +2,7 @@ const { Titlebar, Color } = require ('custom-electron-titlebar');
 
 window.addEventListener('DOMContentLoaded', () => {
   // Title bar implemenation
+    console.log("TICKLE THE PRELOAD");
     new Titlebar({
         backgroundColor: Color.fromHex('#EFEFEF'),
         drag: true
